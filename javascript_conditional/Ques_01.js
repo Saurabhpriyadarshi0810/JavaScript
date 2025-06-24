6cd285d4d7dd91d33b047b9ca5719713059b9647
Saurabhpriyadarshi0810 <spriyadarshi831@gmail.com>
2025-06-24 11:47:37 +0530

    question solved

---
+++ javascript_conditional/Ques_01.js
@@ -0,0 +1,12 @@
+console.log("hello");
+let num;
+
+num = prompt("Enter a number");
+if (num % 5 ===0){
+    console.log(`${num} is a multiple of 5`);
+    alert(`${num} is a multiple of 5`);
+}
+else {
+    console.log(`${num} is not a multiple of 5`);
+    alert(`${num} is not a multiple of 5`);
+}
---
+++ javascript_conditional/Ques_02.js
@@ -0,0 +1,16 @@
+let num = prompt("enter the marks  of exam");
+if ( num >= 80 && num <=100){
+    console.log("A");
+}
+else if ( num >= 70 && num <=89){
+    console.log("B");
+}
+else if ( num >= 60 && num <=79){
+    console.log("C");
+}
+else if ( num >= 50 && num <=69){
+    console.log("D");
+}
+else if ( num >= 0 && num <=49){
+    console.log("F");
+}
