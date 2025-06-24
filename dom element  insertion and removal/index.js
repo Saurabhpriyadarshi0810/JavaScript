3b59fc7c6d2ba488da845f838299565afbe6085f
Saurabhpriyadarshi0810 <spriyadarshi831@gmail.com>
2025-06-24 11:25:14 +0530

    done

---
+++ dom element  insertion and removal/index.js
@@ -0,0 +1,67 @@
+// sam = document.querySelector(".container")
+
+// <div class=​"container">​…​</div>​
+
+// sam.inner
+
+// undefined
+
+// sam.innerHTML
+
+// '\n        <div class="box">\n            i am Saurabh Priyadarshi.\n        </div>\n        <div class="box">\n            i am Saurabh Priyadarshi.\n        </div>\n    '
+
+// sam.innerText
+
+// 'i am Saurabh Priyadarshi.\ni am Saurabh Priyadarshi.'
+
+
+// ram = document.querySelector(".box")
+
+// <div class=​"box">​ i am Saurabh Priyadarshi. ​</div>​
+
+// ram.innerHTML
+
+// '\n            i am Saurabh Priyadarshi.\n        '
+
+// ram.innerText
+
+// 'i am Saurabh Priyadarshi.'
+
+// sam.outerHTML
+
+// '<div class="container">\n        <div class="box">\n            i am Saurabh Priyadarshi.\n        </div>\n        <div class="box">\n            i am Saurabh Priyadarshi.\n        </div>\n    </div>'
+
+// sam.tagName
+
+// 'DIV'
+
+// sam.nodeName
+
+// 'DIV'
+
+// sam.textContent
+
+// '\n        \n            i am Saurabh Priyadarshi.\n        \n        \n            i am Saurabh Priyadarshi.\n        \n    '
+
+// sam.hidden
+
+// false
+
+// sam.hidden = true
+
+// true
+
+// sam.hidden = false 
+
+// false
+
+
+// sam.hidden = true
+// true
+
+// ram.innerHTML ="hey i am archi "
+
+// 'hey i am archi '
+
+// sam.hidden = false
+// false
